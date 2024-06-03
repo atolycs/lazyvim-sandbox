8234e764b1d23cd24e99b658f35557b36aabbfa3
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-06-03 19:14:07 +0900

    feat: add config

---
+++ lua/plugins/colorschmea.lua
@@ -0,0 +1,13 @@
+return {
+  "projekt0n/github-nvim-theme",
+  lazy = true,
+  priority = 1000,
+  config = function()
+    require("github-theme").setup()
+  end,
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
+}
