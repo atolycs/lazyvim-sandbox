d2f8bc6cbd6d021e83bba49328147d399faf3860
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-06-06 18:16:54 +0900

    feat: update opts configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,25 +4,43 @@
 
 local keymap = vim.keymap
 
-local silent_opts = { noremap = true, silent = true }
-local opts = { noremap = true, silent = false }
+local function silent_opts(opts)
+  opts.silent = true
+  opts.noremap = true
+  return opts
+end
 
-keymap.set("n", "dd", '"_dd', { desc = "Delete line" })
-keymap.set("n", "x", '"_x', { desc = "Delete character" })
-keymap.set("n", "qq", ":qa<CR>", { desc = "Quit All" })
+local function default_opts(opts)
+  opts.silent = false
+  opts.noremap = true
+  return opts
+end
 
--- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d', { desc = "Delete backwords" })
+-- local silent_opts = { noremap = true, silent = true, desc = "" }
+-- local opts = { noremap = true, silent = false, desc = "" }
+
+keymap.set("n", "dd", '"_dd', default_opts({ desc = "Delete line" }))
+keymap.set("n", "x", '"_x', default_opts({ desc = "Delete character" }))
+keymap.set("n", "qq", ":qa<CR>", default_opts({ desc = "Quit all" }))
+
+-- Delete a word backwardoptss
+keymap.set("n", "dw", 'vb"_d', default_opts({ desc = "Delete back word" }))
 
 -- select all
-keymap.set("n", "<C-a>", "gg<S-V>G", { desc = "Select all" })
+keymap.set("n", "<C-a>", "gg<S-V>G", default_opts({ desc = "Select all" }))
 
 -- split window
-keymap.set("n", "ss", ":split<CR>", { desc = "Split window" })
-keymap.set("n", "sv", ":vsplit<CR>", { desc = "Vertical split window" })
+keymap.set("n", "ss", ":split<CR>", default_opts({ desc = "Split window" }))
+keymap.set("n", "sv", ":vsplit<CR>", default_opts({ desc = "Vertical split window" }))
 
 --  Move window
-keymap.set("n", "sh", "<C-w>h", silent_opts)
-keymap.set("n", "sk", "<C-w>k", silent_opts)
-keymap.set("n", "sj", "<C-w>j", silent_opts)
-keymap.set("n", "sl", "<C-w>l", silent_opts)
+keymap.set("n", "sh", "<C-w>h", silent_opts({ desc = "Move to the left window" }))
+keymap.set("n", "sk", "<C-w>k", silent_opts({ desc = "Move to the top window" }))
+keymap.set("n", "sj", "<C-w>j", silent_opts({ desc = "Move to the bottom window" }))
+keymap.set("n", "sl", "<C-w>l", silent_opts({ desc = "Move to the right window" }))
+
+-- Delete Window keymaps
+keymap.del("n", "<C-j>")
+keymap.del("n", "<C-k>")
+keymap.del("n", "<C-h>")
+keymap.del("n", "<C-l>")
