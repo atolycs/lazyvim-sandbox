2b577681b81c6d9150a1f1db53ef48afe23b5571
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-06-12 15:43:35 +0900

    feat(keymap): add vscode keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
----- Keymaps are automatically loaded on the VeryLazy even0
+-- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps hereby
 
@@ -34,6 +34,9 @@ keymap.set("n", "q", function(n)
     vim.cmd("helpclose")
   elseif vim.opt.diff:get() then
     vim.cmd("quitall")
+  elseif vim.g.vscode then
+    local vscode = require("vscode")
+    vscode.call("workbench.action.closeActiveEditor")
   else
     LazyVim.ui.bufremove(n)
   end
@@ -42,15 +45,11 @@ end, default_opts({ desc = "Close Tab" }))
 keymap.set("n", "dw", 'vb"_d', default_opts({ desc = "Delete back word" }))
 
 -- Word movement
-keymap.set("n", "<C-l>", "e", default_opts({ desc = "Move to the end of the word" }))
-keymap.set("n", "<C-h>", "b", default_opts({ desc = "Move to the beginning of the word" }))
-keymap.set("v", "<C-l>", "e", default_opts({ desc = "Move to the end of the word" }))
-keymap.set("v", "<C-h>", "b", default_opts({ desc = "Move to the beginning of the word" }))
+keymap.set({ "n", "v" }, "<C-l>", "e", default_opts({ desc = "Move to the end of the word" }))
+keymap.set({ "n", "v" }, "<C-h>", "b", default_opts({ desc = "Move to the beginning of the word" }))
 
 -- Disable continuation of comments
 keymap.set("n", "<Leader>o", "o<Esc>^Da", default_opts({ desc = "Insert newline below" }))
-keymap.set("n", "<C-l>", "e", default_opts({ desc = "Move to the end of the word" }))
-keymap.set("n", "<C-h>", "b", default_opts({ desc = "Move to the beginning of the word" }))
 keymap.set("n", "<Leader>O", "O<Esc>^Da", default_opts({ desc = "Insert newline above" }))
 
 -- select all
@@ -62,6 +61,6 @@ keymap.set("n", "sv", "<cmd>vsplit<CR>", silent_opts({ desc = "Vertical split wi
 
 --  Move window
 keymap.set("n", "sh", "<C-w>h", silent_opts({ desc = "Move to the left window" }))
-keymap.set("n", "sk", "<C-w>k", silent_opts({ desc = "Move to the top window" }))
 keymap.set("n", "sj", "<C-w>j", silent_opts({ desc = "Move to the bottom window" }))
+keymap.set("n", "sk", "<C-w>k", silent_opts({ desc = "Move to the top window" }))
 keymap.set("n", "sl", "<C-w>l", silent_opts({ desc = "Move to the right window" }))
