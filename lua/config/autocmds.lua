5146cee1bfa6991096e979c227847a2f74991b22
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-11-08 12:41:57 +0900

    feat: setup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("atolycs_" .. name, { clear = true })
+end
+
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "markdown" },
   callback = function()
